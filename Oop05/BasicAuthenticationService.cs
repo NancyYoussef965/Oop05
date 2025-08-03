f634b6eeb71b439b2fe06cc899f2f0db036f7a85
Nancy <ny6049812@gmail.com>
2025-08-03 22:24:02 +0300

    Q2

---
+++ Oop05/BasicAuthenticationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop05
+{
+    internal class BasicAuthenticationService : IAuthenticationService
+    {
+        private Dictionary<string, (string Password, string Role)> users = new Dictionary<string, (string, string)>
+    {
+        { "admin", ("1234", "Admin") },
+        { "user1", ("abcd", "User") },
+        { "manager", ("pass", "Manager") }
+    };
+
+        public bool AuthenticateUser(string username, string password)
+        {
+            if (users.ContainsKey(username) && users[username].Password == password)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AuthorizeUser(string username, string role)
+        {
+            if (users.ContainsKey(username) && users[username].Role == role)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Oop05/Program.cs
+++ Oop05/Program.cs
@@ -7,17 +7,42 @@ namespace Oop05
         static void Main(string[] args)
         {
             #region Q1
-            ICircle circle = new Circle(5);
-            IRectangle rectangle = new Rectangle(4, 6);
+            //ICircle circle = new Circle(5);
+            //IRectangle rectangle = new Rectangle(4, 6);
 
-            circle.DisplayShaoedInfo();
-            rectangle.DisplayShaoedInfo();
+            //circle.DisplayShaoedInfo();
+            //rectangle.DisplayShaoedInfo();
             #endregion
 
 
 
 
+            #region Q2
+            //IAuthenticationService authService = new BasicAuthenticationService();
 
+            //string username = "admin";
+            //string password = "1234";
+            //string role = "Admin";
+
+            //if (authService.AuthenticateUser(username, password))
+            //{
+            //    Console.WriteLine("User authenticated.");
+
+            //    if (authService.AuthorizeUser(username, role))
+            //    {
+            //        Console.WriteLine("User authorized as " + role);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("User is not authorized as " + role);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Authentication failed.");
+            //}
+
+            #endregion
 
 
         }
